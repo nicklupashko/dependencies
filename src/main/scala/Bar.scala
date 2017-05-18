6a9bbe35f33e0ebad09f1a3fbfc04beb10a42e70
Nick Lupashko <n.r.lupashko@gmail.com>
2017-05-18 03:11:29 +0300

    UI - Bar

--- src/main/scala/Bar.scala
+++ src/main/scala/Bar.scala
@@ -1,22 +1,26 @@
-import scalafx.geometry.Pos
+import scala.concurrent.Future
+import scala.concurrent
+.ExecutionContext.Implicits.global
+import scalafx.geometry.Insets
 import scalafx.scene.control._
 import scalafx.scene.layout.Region
 import scalafx.scene.paint.Color
 import scalafx.scene.text.Font
 
 object Bar {
-  def get = toolBar
+  def get: ToolBar = toolBar
 
   private val label = new Label {
     text = "Dependencies"
     font = new Font("Helvetica", 18)
     textFill = Color.web("#e6e6e6")
-    alignment = Pos.Center
+    padding  = Insets(0, 8, 0, 4)
+    margin   = Insets(0, 0, 4, 0)
   }
 
   private val textField = new TextField {
     promptText = "Path to files directory"
-    minWidth = 200
+    minWidth   = 200
   }
 
   private val tog = new ToggleGroup()
@@ -25,17 +29,50 @@ object Bar {
     textField.promptText = s"Path to ${ext} files directory"
   })
 
+  private def radioButton
+  (txt: String, select: Boolean = false): RadioButton = {
+    new RadioButton {
+      text     = txt
+      selected = select
+      padding  = Insets(0, 0, 0, 6)
+      toggleGroup = tog
+    }
+  }
+
+  private val processingLabel = new Label {
+    text = "Processing"
+    font = new Font("Helvetica", 12)
+    textFill = Color.web("#2BB2DD")
+    padding  = Insets(0, 8, 0, 8)
+    margin   = Insets(0, 0, 2, 0)
+    visible  = false
+  }
+
+  private val progressBar = new ProgressBar {
+    prefWidth = 170
+    padding   = Insets(0, 4, 0, 4)
+    visible   = false
+  }
+
   private val button = new Button {
-    minWidth = 40
     text = "Let's go"
+    minWidth = 40
     onAction = _ => {
-      val list =
-        tog.selectedToggle.value
-          .asInstanceOf[javafx.scene.control.RadioButton].getText match {
-          case ".class" => Dependencies.fromClassFiles(textField.text.value)
-          case ".java"  => Dependencies.fromJavaFiles(textField.text.value)
-        }
-      Frame.updateList(list)
+      progressBar.visible     = true
+      processingLabel.visible = true
+
+      val future = Future {
+        val path: String = textField.text.value
+        val extension: String = tog.selectedToggle.value
+          .asInstanceOf[javafx.scene.control.RadioButton].getText
+        Frame.updateList(path, extension)
+      }
+
+      future.onComplete {
+        case _ =>
+          progressBar.visible     = false
+          processingLabel.visible = false
+      }
     }
   }
 
@@ -44,23 +81,22 @@ object Bar {
     style = "-fx-base: #333333"
     prefHeight = 30
     content = List(
-      label, region(20),
-      textField, region(10),
-      new RadioButton {
-        selected = true
-        text = ".class"
-        toggleGroup = tog
-      },
-      new RadioButton {
-        text = ".java"
-        toggleGroup = tog
-      },
-      region(435),
+      label,
+      textField,
+      radioButton(".class", true),
+      radioButton(".java"),
+      region(200),
+      processingLabel,
+      progressBar,
+      region(8),
       button
     )
   }
 
   private def region(wid: Double): Region = {
-    new Region { prefWidth = wid }
+    new Region {
+      prefWidth = wid
+    }
   }
+
 }
