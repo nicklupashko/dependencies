6f8a4ecb2f44d2479cb4356b13355bdcb86b9527
Nick Lupashko <n.r.lupashko@gmail.com>
2017-05-21 00:24:03 +0300

    UI - [Frame] Work in progress

--- src/main/scala/Frame.scala
+++ src/main/scala/Frame.scala
@@ -1,27 +1,28 @@
 import scalafx.application.JFXApp
 import scalafx.scene._
 import scalafx.stage.StageStyle
-import scalafx.scene.layout.{VBox}
+import scalafx.scene.layout.BorderPane
 import member._;
 
 object Frame extends JFXApp {
   private var list: List[DClass] = List.empty[DClass]
 
-  def updateList(newList: List[DClass]): Unit =
-    this.list = newList
+  def updateList(path: String, extension: String): Unit = {
+    list = extension match {
+      case ".class" => Dependencies.fromClassFiles(path)
+      case ".java"  => Dependencies.fromJavaFiles(path)
+    }
+  }
 
   stage = new JFXApp.PrimaryStage {
     title = "Dependencies"
     initStyle(StageStyle.Unified)
     resizable = false
-    width = 980
+    width  = 980
     height = 680
     scene = new Scene {
-      root = new VBox {
-        spacing = 5
-        children = List(
-          Bar.get
-        )
+      root = new BorderPane {
+        top = Bar.get
       }
     }
 
