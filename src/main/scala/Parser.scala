fa8b4b23f3e19444874116ddbbf928ba54dfcec5
Nick Lupashko <n.r.lupashko@gmail.com>
2017-05-20 23:17:46 +0300

    [Parser] Add implicit class to avoid asInstanceOf

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -8,28 +8,21 @@ import scala.reflect.io.File
 object Parser {
 
   def classFileToDClass(file: File): DClass = {
-    val reader = new ClassReader(file.toByteArray)
     val node = new ClassNode()
+    val reader = new ClassReader(file.toByteArray)
     reader.accept(node, 0)
     classNodeToDClass(node)
   }
 
-  private type AL[T] = java.util.ArrayList[T]
-
-  private val primitives: Set[String] = Set("B", "C", "D", "F", "J", "I", "S", "Z")
-
   private def classNodeToDClass(node: ClassNode): DClass = {
-    val interfaces: List[String] =
-      node.interfaces.asInstanceOf[AL[String]].asScala.toList
+    val interfaces: List[String] = node.interfaces.toScalaList[String]
 
-    val fields: List[DField] =
-      node.fields.asInstanceOf[AL[FieldNode]].asScala.toList
-        .map(fld => DField(node.name, fld.name, fld.desc))
+    val fields: List[DField] = node.fields.toScalaList[FieldNode]
+      .map(fld => DField(node.name, fld.name, fld.desc))
 
-    val methods: List[DMethod] =
-      node.methods.asInstanceOf[AL[MethodNode]].asScala.toList
-        .filterNot(_.name.matches("<[a-z]+>"))
-        .map(mtd => methodNodeToDMethod(node.name, mtd))
+    val methods: List[DMethod] = node.methods.toScalaList[MethodNode]
+      .filterNot(_.name.matches("<[a-z]+>"))
+      .map(mtd => methodNodeToDMethod(node.name, mtd))
 
     DClass(node.name, node.superName, interfaces, fields, methods)
   }
@@ -51,14 +44,18 @@ object Parser {
       case _ => None
     })
 
-    Option(node.localVariables.asInstanceOf[AL[LocalVariableNode]]) match {
-      case Some(vars) => vars.asScala
-        .filterNot(v => v.name.matches("this") || primitives.contains(v.desc.replaceAll("\\[", "")))
-        .foreach(v => refs += DRef("V", s"${v.name}: ${v.desc}"))
-      case None => None
-    }
+    Option(node.localVariables.toArrayList[LocalVariableNode]).map(_.asScala
+      .filterNot(v => v.name.matches("this") || primitives.contains(v.desc.replaceAll("\\[", "")))
+      .foreach(v => refs += DRef("V", s"${v.name}: ${v.desc}")))
 
     DMethod(owner, node.name, node.desc, refs.toList)
+  }
+
+  private val primitives: Set[String] = Set("B", "C", "D", "F", "J", "I", "S", "Z")
+
+  private implicit class List2ArrayList[T](list: java.util.List[T]) {
+    def toArrayList[A] = list.asInstanceOf[java.util.ArrayList[A]]
+    def toScalaList[A] = list.toArrayList[A].asScala.toList
   }
 
 }
