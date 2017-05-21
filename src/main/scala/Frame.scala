9f7153666be220ac1cbdd840962d421eeccfce16
Nick Lupashko <n.r.lupashko@gmail.com>
2017-05-21 23:00:17 +0300

    UI - [Frame] Work in progress 4

--- src/main/scala/Frame.scala
+++ src/main/scala/Frame.scala
@@ -20,12 +20,33 @@ object Frame extends JFXApp {
 
   def updateTreeView(name: String, list: List[DClass]): Unit = {
     val root = new TreeItem[String](name)
-    root.children = list.map(n => {
-      val item = new TreeItem[String](n.name)
-      item.children =
-        n.fields.map(f => new TreeItem[String](f.name)) ++
-          n.methods.map(m => new TreeItem[String](m.name))
-      item
+
+    root.children = list.map(node => {
+      val classItem = new TreeItem[String](node.name)
+
+      val fieldItems = new TreeItem[String]("Fields:")
+      fieldItems.children = node.fields.map(fld =>
+        new TreeItem[String](s"${fld.name}: ${fld.desc.replaceAll(".+/(.+);", "$1")}")
+      )
+
+      val methodItems = new TreeItem[String]("Methods:")
+      methodItems.children =
+        node.methods.map(mtd => {
+          val metItem = new TreeItem[String](s"${mtd.name}")
+          metItem.children = mtd.refs.map(ref => {
+            val refOpt = ref.flag match {
+              case "F" => ref.reference.replaceAll(".+/(.+)", "$1")
+              case "M" => ref.reference.replaceAll(".+/(.+) .+", "$1(args)")
+              case "V" => ref.reference.replaceAll("(.+:) .+/(.+);", "$1 $2")
+            }
+            new TreeItem[String](s"${ref.flag} ${refOpt}")
+          })
+          metItem
+        })
+
+      classItem.children = List(fieldItems, methodItems)
+
+      classItem
     })
 
     tree.root = root
