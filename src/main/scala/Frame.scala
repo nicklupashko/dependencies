6a32c3be7cd1ed0e9b94c6b2d18c40b782916a8b
Nick Lupashko <n.r.lupashko@gmail.com>
2017-06-03 23:29:55 +0300

    UI - [Frame] Work in progres 5

--- src/main/scala/Frame.scala
+++ src/main/scala/Frame.scala
@@ -1,28 +1,33 @@
 import scalafx.application.{JFXApp, Platform}
 import scalafx.scene._
+import scalafx.Includes._
 import scalafx.stage.StageStyle
 import scalafx.scene.layout.BorderPane
 import scalafx.scene.control.{TreeItem, TreeView}
 import member._
 
+import scala.reflect.io.Path
+
 object Frame extends JFXApp {
 
-  private val tree = new TreeView[String](new TreeItem[String](""))
+  private val tree = new TreeView[String] {
+    focusTraversable = false
+    root = new TreeItem[String]("There's nothing to show")
+  }
 
   def update(path: String, extension: String): Unit = {
     val list = extension match {
       case ".class" => Dependencies.fromClassFiles(path)
       case ".java"  => Dependencies.fromJavaFiles(path)
     }
-    val name = path.replaceAll(".+/(.+)", "$1")
-    Platform.runLater(updateTreeView(name, list))
+    Platform.runLater(updateTreeView(Path(path).name, list))
   }
 
   def updateTreeView(name: String, list: List[DClass]): Unit = {
     val root = new TreeItem[String](name)
 
     root.children = list.map(node => {
-      val classItem = new TreeItem[String](node.name)
+      val classItem = new TreeItem[String](node.name.replaceAll(".+/(.+)", "$1"))
 
       val fieldItems = new TreeItem[String]("Fields:")
       fieldItems.children = node.fields.map(fld => {
@@ -31,21 +36,10 @@ object Frame extends JFXApp {
 
       val methodItems = new TreeItem[String]("Methods:")
       methodItems.children = node.methods.map(mtd => {
-        val metItem = new TreeItem[String](s"${mtd.name}")
-        metItem.children = mtd.refs.map(ref => {
-          val refOpt = ref.flag match {
-            case "F" => ref.reference.replaceAll(".+/(.+)", "$1")
-            case "M" => ref.reference.replaceAll(".+/(.+) .+", "$1(args)")
-            case "V" => ref.reference.replaceAll("(.+:) .+/(.+);", "$1 $2")
-          }
-          new TreeItem[String](s"${ref.flag} ${refOpt}")
-        })
-
-        metItem
+        new TreeItem[String](s"${mtd.name}")
       })
 
-      classItem.children =
-        List(fieldItems, methodItems)
+      classItem.children = List(fieldItems, methodItems)
       classItem
     })
 
@@ -59,9 +53,12 @@ object Frame extends JFXApp {
     height = 680
     resizable = false
     scene = new Scene {
+      stylesheets = List(
+        getClass.getResource("style.css")
+          .toExternalForm)
       root = new BorderPane {
         top = Bar.get
-        center = tree
+        left = tree
       }
     }
   }
