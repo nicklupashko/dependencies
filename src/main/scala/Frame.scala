5c586946366843557c1e6fbc1941badfb6e70ff9
Nick Lupashko <n.r.lupashko@gmail.com>
2017-06-06 13:17:24 +0300

    [Frame] Update

--- src/main/scala/Frame.scala
+++ src/main/scala/Frame.scala
@@ -1,3 +1,6 @@
+import javafx.scene.text.Text
+import javax.swing.SwingUtilities
+
 import scalafx.application.{JFXApp, Platform}
 import scalafx.scene._
 import scalafx.stage.StageStyle
@@ -5,36 +8,94 @@ import scalafx.scene.layout.BorderPane
 import scalafx.scene.control.{TreeItem, TreeView}
 import member._
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.reflect.io.Path
+import scalafx.embed.swing.SwingNode
 
 object Frame extends JFXApp {
+
+  private var classList = List.empty[DClass]
+
+  private val bar = Bar.toolBar
+
+  private val graph = new SwingNode
 
   private val tree = new TreeView[String] {
     focusTraversable = false
     root = new TreeItem[String]("There's nothing to show")
   }
+  tree.onMouseClicked = e => {
+    val node = e.getPickResult().getIntersectedNode()
+    if (e.getClickCount == 2 && node.isInstanceOf[Text]) {
+      val nodeName = tree.getSelectionModel.getSelectedItem.getValue
+
+      Bar.progressBar.visible = true
+      Bar.processingLabel.visible = true
+
+      println(s"*** $nodeName ***")
+      val future = Future {
+        SwingUtilities.invokeAndWait(() =>
+          updateGraph(findAllOfIt(nodeName)))
+      }
+
+      future onComplete {
+        case _ =>
+          Bar.progressBar.visible = false
+          Bar.processingLabel.visible = false
+      }
+    }
+  }
+
+  def findAllOfIt(methodName: String): List[String] = {
+    var i = 0
+    val allMethods = classList.flatMap(_.methods)
+
+    def findMethod(methodName: String): Option[DMethod] =
+      allMethods.find(m => (m.name + m.desc) == methodName)
+
+    def isMethodExists(methodName: String): Boolean =
+      allMethods.exists(m => (m.name + m.desc) == methodName)
+
+    findMethod(methodName) match {
+      case None => List.empty[String]
+      case Some(m) =>
+        val fields: List[String] = m.refs.filter(_.flag == "F")
+          .map(f => s"$methodName^${f.flag} ${f.reference}")
+        val vars: List[String] = m.refs.filter(_.flag == "V")
+          .map(v => s"${v.flag} ${v.reference}")
+          .map(_.replaceAll("(.+):", "$1(" + {i = i + 1; i} + "):"))
+          .map(v => s"$methodName^$v")
+        val methods: List[String] = m.refs.filter(_.flag == "M")
+          .filter(m => isMethodExists(m.reference))
+          .map(m => s"$methodName^${m.reference}")
+
+        fields ++ vars ++ methods ++
+          m.refs.filter(_.flag == "M").map(_.reference).flatMap(findAllOfIt)
+    }
+  }
 
   def update(path: String, extension: String): Unit = {
-    val list = extension match {
+    classList = extension match {
       case ".class" => Dependencies.fromClassFiles(path)
       case ".java"  => Dependencies.fromJavaFiles(path)
     }
-    Platform.runLater(updateTreeView(Path(path).name, list))
+    Platform.runLater(updateTreeView(Path(path).name, classList))
+  }
+
+  def updateGraph(list: List[String]): Unit = {
+    import scala.collection.JavaConverters._
+    graph.content = Graph343.node(list.asJava)
   }
 
   def updateTreeView(name: String, list: List[DClass]): Unit = {
     tree.root = new TreeItem[String] {
       value = name
-      children = list.map(node => {
+      children = list.map(clazz => {
         new TreeItem[String] {
-          value = node.name.replaceFirst(".+/(.+)", "$1").replaceAll("\\$", ".")
-          children = node.methods.map(mtd => {
-            val params = mtd.desc.replaceFirst("\\((.+)?\\).+", "$1").split(";")
-              .map(_.replaceFirst("(\\[*)?.+/(.+)", "$2$1")).mkString(", ")
-            val retType = mtd.desc.replaceFirst(".+\\)(\\[*)?(.+/)?(.+)", "$3$1")
-            new TreeItem[String](s"${mtd.name}($params): $retType"
-              .replaceAll("\\$", ".").replaceAll(";", "").replaceAll("\\[", "[]"))
-          })
+          value = clazz.name
+          children = clazz.methods.map(method =>
+            new TreeItem[String](method.name + method.desc))
         }
       })
     }
@@ -48,8 +109,9 @@ object Frame extends JFXApp {
     resizable = false
     scene = new Scene {
       root = new BorderPane {
-        top = Bar.get
+        top = bar
         left = tree
+        center = graph
       }
     }
   }
